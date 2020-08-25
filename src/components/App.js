dd214c0a429874b1c7d4c6e39acd0177e60a9f04
Pablomombo <Pablomombo@gmail.com>
2020-08-25 15:54:09 +0200

    search complete

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
-import {without} from 'lodash';
+import {findIndex, without} from 'lodash';
 
 class App extends Component {
 
@@ -14,16 +14,45 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+
   }
 
   toggleForm() {
     this.setState({
       formDisplay: !this.state.formDisplay
+    });
+  }
+
+  searchApts(query) {
+    this.setState({queryText: query});
+  }
+
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex =  findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
     });
   }
 
@@ -63,6 +92,36 @@ class App extends Component {
 
   render() {
 
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts = filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+      ) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    }).filter(eachItem => {
+      return(
+        eachItem['petName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['ownerName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase()) ||
+        eachItem['aptNotes']
+        .toLowerCase()
+        .includes(this.state.queryText.toLowerCase())
+      );
+    });
+
     return (
       <main className="page bg-white" id="petratings">
       <div className="container">
@@ -74,9 +133,16 @@ class App extends Component {
                 toggleForm={this.toggleForm}
                 addAppointment = {this.addAppointment}
               />
-              <SearchAppointments />
-              <ListAppointments appointments={this.state.myAppointments}
-                deleteAppointment={this.deleteAppointment} />
+              <SearchAppointments 
+                orderBy={this.state.orderBy}
+                orderDir={this.state.orderDir}
+                changeOrder={this.changeOrder}
+                searchApts={this.searchApts}
+              />
+              <ListAppointments appointments={filteredApts}
+                deleteAppointment={this.deleteAppointment}
+                updateInfo={this.updateInfo}
+              />
             </div>
           </div>
         </div>
