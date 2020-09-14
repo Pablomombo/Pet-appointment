0b394d16245c75221ec05c44f8c323032e22e8e2
Pablomombo <Pablomombo@gmail.com>
2020-09-14 20:51:49 +0200

    list appointment

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import { FaPlus } from 'react-icons/fa';
 import { FaTimes } from 'react-icons/fa';
 import Moment from 'react-moment';
 
 class ListAppointments extends Component {
     render() {
-        return (
+        return ( 
             <div className="appointment-list item-list mb-3">
+                <div className="apt-addheading card-header bg-primary text-white">
+                    List Appointment
+                </div>
+                
                 {this.props.appointments.map( item => (
                     <div className="pet-item col media py-3" key={item.aptId}>
                     <div className="mr-3">
