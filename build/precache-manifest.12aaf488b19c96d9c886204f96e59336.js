f9fa249d02493bb7a6ca1f59ddb33b105d2cd5c3
Pablomombo <Pablomombo@gmail.com>
2020-09-15 12:28:04 +0200

    done

---
+++ build/precache-manifest.12aaf488b19c96d9c886204f96e59336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c3f7471a4e706f419b1b69021d7dcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1660d88485553d3b760e",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "12a725652766500a8662",
+    "url": "/static/css/main.255d1621.chunk.css"
+  },
+  {
+    "revision": "1660d88485553d3b760e",
+    "url": "/static/js/2.4cd11758.chunk.js"
+  },
+  {
+    "revision": "12f5fece24482f5e949c183d0aad3b8d",
+    "url": "/static/js/2.4cd11758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a725652766500a8662",
+    "url": "/static/js/main.2640de21.chunk.js"
+  },
+  {
+    "revision": "425c762e9c40c6d032e6",
+    "url": "/static/js/runtime-main.aaa5a838.js"
+  }
+]);
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -6,62 +6,64 @@ import Moment from 'react-moment';
 class ListAppointments extends Component {
     render() {
         return ( 
-            <div className="appointment-list item-list mb-3">
-                <div className="apt-addheading card-header bg-primary text-white">
-                    List Appointment
-                </div>
-                
-                {this.props.appointments.map( item => (
-                    <div className="pet-item col media py-3" key={item.aptId}>
-                    <div className="mr-3">
-                        <button className="pet-delete btn btn-sm btn-danger"
-                        onClick={()=> this.props.deleteAppointment(item)}>
-                            <FaTimes />
-                        </button>
+            <React.Fragment>
+                <div className="appointment-list item-list mb-3">
+                    <div className="apt-addheading card-header bg-primary text-white">
+                        List Appointment
                     </div>
-        
-                    <div className="pet-info media-body">
-                        <div className="pet-head d-flex">
-                            <span className="pet-name"
-                                contentEditable
-                                suppressContentEditableWarning
-                                onBlur={
-                                    e => this.props.updateInfo('petName', e.target.innerText, item.aptId)
-                                }
-                            >{item.petName}</span>
-                            <span className="apt-date ml-auto">
-                                <Moment
-                                    date={item.aptDate}
-                                    parse="YYYY-MM-dd hh:mm"
-                                    format="MMM-D h:mma"
-                                />
-                            </span>
+
+                    {this.props.appointments.map( item => (
+                        <div className="pet-item col media py-3" key={item.aptId}>
+                        <div className="mr-3">
+                            <button className="pet-delete btn btn-sm btn-danger"
+                            onClick={()=> this.props.deleteAppointment(item)}>
+                                <FaTimes />
+                            </button>
                         </div>
-        
-                        <div className="owner-name">
-                            <span className="label-item">Owner : </span>
-                            <span
-                                contentEditable
-                                suppressContentEditableWarning
-                                onBlur={
-                                    e => this.props.updateInfo('ownerName', e.target.innerText, item.aptId)
-                                }
-                            >{item.ownerName}</span>
-                        </div>
-                        <div className="apt-notes">
-                            <span className="label-item">Notes : </span>
-                            <span
-                                contentEditable
-                                suppressContentEditableWarning
-                                onBlur={
-                                    e => this.props.updateInfo('aptNotes', e.target.innerText, item.aptId)
-                                }
-                            >{item.aptNotes}</span>
+            
+                        <div className="pet-info media-body">
+                            <div className="pet-head d-flex">
+                                <span className="pet-name"
+                                    contentEditable
+                                    suppressContentEditableWarning
+                                    onBlur={
+                                        e => this.props.updateInfo('petName', e.target.innerText, item.aptId)
+                                    }
+                                >{item.petName}</span>
+                                <span className="apt-date ml-auto">
+                                    <Moment
+                                        date={item.aptDate}
+                                        parse="YYYY-MM-dd hh:mm"
+                                        format="MMM-D h:mma"
+                                    />
+                                </span>
+                            </div>
+            
+                            <div className="owner-name">
+                                <span className="label-item">Owner : </span>
+                                <span
+                                    contentEditable
+                                    suppressContentEditableWarning
+                                    onBlur={
+                                        e => this.props.updateInfo('ownerName', e.target.innerText, item.aptId)
+                                    }
+                                >{item.ownerName}</span>
+                            </div>
+                            <div className="apt-notes">
+                                <span className="label-item">Notes : </span>
+                                <span
+                                    contentEditable
+                                    suppressContentEditableWarning
+                                    onBlur={
+                                        e => this.props.updateInfo('aptNotes', e.target.innerText, item.aptId)
+                                    }
+                                >{item.aptNotes}</span>
+                            </div>
                         </div>
                     </div>
+                    ))}
                 </div>
-                ))}
-            </div>
+            </React.Fragment>
         );
     }
 }
